7f9945eebb77cc4f61e7a43614e707e7c61fd368
«Max» <supreme14@rambler.ru>
2022-06-22 10:29:31 +0300

    privetstvie

---
+++ example005_privetstvie/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("имя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+
+    Console.WriteLine("Привет сука Маша");
+
+else
+
+    Console.WriteLine("Привет");
+    Console.WriteLine(username);
+  
+    
+        
+    
+
+    
+
+
+
